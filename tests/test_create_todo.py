2c6cf428c7dc94a049d2a7249920a2388851995b
Daniel <gleek.dan@gmail.com>
2021-04-27 21:41:17 -0600

    Add test_new_todo_raises_not_authenticated.  update .gitignore

--- tests/test_create_todo.py
+++ tests/test_create_todo.py
@@ -23,3 +23,21 @@ def test_create_new_todo(login, client):
     assert body['complete'] == False
     assert body['id'] == 1
     assert body['owner_id'] == 1
+
+
+@pytest.mark.todos
+def test_create_new_todo_raises_not_authenticated(client):
+    # Arrange
+    url = '/create-todo'
+    payload = {
+        "title": "Buy Milk",
+        "complete": False
+    }
+
+    # Act
+    response = client.post(url, json=payload)
+    body = response.json()
+
+    # Assert
+    assert response.status_code == 401
+    assert body["detail"] == "Not authenticated"
