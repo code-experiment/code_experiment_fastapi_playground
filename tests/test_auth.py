02e57e526752d7dc1095e1656ee713b80afc1fe9
Daniel <gleek.dan@gmail.com>
2021-04-27 19:52:00 -0600

    Add test_login_raises_incorrect_username

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -18,3 +18,21 @@ def test_login(test_db_session, create_single_user, client):
     assert response.status_code == 200
     assert "access_token" in body
     assert body["token_type"] == "bearer"
+
+
+@pytest.mark.auth
+def test_login_raises_incorrect_username(client):
+    # Arrange
+    url = '/login'
+    payload = {
+        "username": "test1",
+        "password": "test1"
+    }
+
+    # Act
+    response = client.post(url, data=payload)
+    body = response.json()
+
+    # Assert
+    assert response.status_code == 404
+    assert body["detail"] == "Incorrect Username"
