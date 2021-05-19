1ef839d2980f602476a3b4bc0bdfb01631f45f7e
Daniel <gleek.dan@gmail.com>
2021-05-18 20:15:05 -0600

    Format todo route

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -15,6 +15,7 @@ def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db), current
 def get_todos(db: Session = Depends(get_db), current_user: schemas.User = Depends(authentication.get_current_user)):
     return crud.get_todos(db=db, user_id=current_user.id)
 
+
 @router.patch("/toggle-complete/{todo_id}", response_model=schemas.Todo)
 def toggle_complete(todo_id: int, db: Session = Depends(get_db),  current_user: schemas.User = Depends(authentication.get_current_user)):
     return crud.toggle_complete(db=db, todo_id=todo_id)
