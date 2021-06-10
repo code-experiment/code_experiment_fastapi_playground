230670df6c7a15f71983b4e4d51b568e8e2401a5
Daniel <gleek.dan@gmail.com>
2021-06-09 19:05:18 -0600

    Remove check_same_thread and timeout as these fixed issues with sqlite3

--- app/database.py
+++ app/database.py
@@ -6,16 +6,18 @@ from sqlalchemy.orm import sessionmaker
 print(os.getenv("DATABASE_URL"))
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
+        "postgres://", "postgresql://", 1)
 # SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 # connect_args might not be needed
-# check_same_thread was a fix for an error during testing
+# check_same_thread was a fix for an error during testing on github
 # timeout was a fix for an error during heroku build for sqlite
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False, 'timeout': 15}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False, 'timeout': 15}
+# )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
