b1ef8430acdb9ee69d1b5ffff0135530652c324e
Daniel <gleek.dan@gmail.com>
2021-06-09 19:25:22 -0600

    Cleanup database file

--- app/database.py
+++ app/database.py
@@ -3,13 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-print(os.getenv("DATABASE_URL"))
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
         "postgres://", "postgresql://", 1)
-# SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 # connect_args might not be needed
 # check_same_thread was a fix for an error during testing on github
