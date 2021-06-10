81ead4afea52d2b2016e51527bbc4dbc3f8e4f09
Daniel <gleek.dan@gmail.com>
2021-06-09 18:46:22 -0600

    Update database file to search for DATABASE_URL environment variable for postgres prep

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+print(os.getenv("DATABASE_URL"))
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
+# SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 # connect_args might not be needed
