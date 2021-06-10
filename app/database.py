ca4e83f3a330a70eebc852bbad2502dce6808aea
Daniel <gleek.dan@gmail.com>
2021-06-09 18:51:49 -0600

    Add fix for heroku postgres working with sqlalchemy >= 1.4.x

--- app/database.py
+++ app/database.py
@@ -3,9 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 print(os.getenv("DATABASE_URL"))
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 # SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
